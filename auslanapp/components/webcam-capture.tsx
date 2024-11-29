098614d978bfaa839b878ead2d65f7e25905edf8
EricKim204 <ericmhkim9724@gmail.com>
2024-11-29 16:57:23 +1100

    ESLint Bugfixes3 removed useState from webcam capture

--- auslanapp/components/webcam-capture.tsx
+++ auslanapp/components/webcam-capture.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect} from 'react';
 import { useWebcam } from '@/lib/hooks/useWebcam';
 import { useMediaPipeModel } from '@/lib/hooks/useMediaPipeModel';
 import { useTensorFlowModel } from '@/lib/hooks/useTensorFlowModel';
